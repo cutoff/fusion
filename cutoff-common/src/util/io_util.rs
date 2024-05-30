298532f2bf51d32131edac141f2f37ab8881bac9
Renaud Denis <mail@renauddenis.com>
2024-05-30 18:24:56 +0200

    Introduce helper function for recursively creating directories of a file

--- cutoff-common/src/util/io_util.rs
+++ cutoff-common/src/util/io_util.rs
@@ -1,6 +1,15 @@
+use std::fs::create_dir_all;
+use std::io;
 use std::path::PathBuf;
 
 use glob::glob;
+
+pub fn create_dir_all_for(path: PathBuf) -> io::Result<()> {
+    if let Some(parent) = path.parent() {
+        create_dir_all(parent)?;
+    }
+    Ok(())
+}
 
 pub fn get_matching_paths_by_glob_pattern(glob_pattern: &str) -> Result<Vec<PathBuf>, glob::PatternError> {
     let mut paths = vec![];
