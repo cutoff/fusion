461c9eeff69d40655e4e68c329966e88d2871817
Renaud Denis <mail@renauddenis.com>
2024-07-12 17:01:22 +0200

    `init_logging` now accepts a max_level

--- cutoff-common/src/util.rs
+++ cutoff-common/src/util.rs
@@ -2,12 +2,11 @@ pub mod buffer_util;
 pub mod lang_util;
 pub mod io_util;
 
-
 /// Just a common way of initializing the logging infrastructure
-pub fn init_logging() {
+pub fn init_logging(max_level: tracing::Level) {
     tracing_subscriber::fmt()
         .compact()
-        .with_max_level(tracing::Level::TRACE)
+        .with_max_level(max_level)
         // .without_time()
         // disable printing the name of the module in every log line.
         .with_target(false)
