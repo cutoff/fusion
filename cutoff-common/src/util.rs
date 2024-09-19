afa31475ac9d649ac86790a5040a16cf9db42a91
Renaud Denis <mail@renauddenis.com>
2024-09-19 16:43:31 +0200

    Fine-tune default logging configuration: display target

--- cutoff-common/src/util.rs
+++ cutoff-common/src/util.rs
@@ -10,7 +10,7 @@ pub fn init_logging(max_level: tracing::Level) {
         .with_max_level(max_level)
         // .without_time()
         // disable printing the name of the module in every log line.
-        .with_target(false)
+        .with_target(true)
         .with_thread_names(true)
         .with_thread_ids(false)
         // disabling coloring, which won't work well in non-display logs
