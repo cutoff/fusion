58ca25ba9fafa47be48f271f9c46b431b1cad212
Renaud Denis <mail@renauddenis.com>
2024-10-08 17:46:21 +0200

    Introduce IntoOk trait for any type

--- cutoff-common/src/lib.rs
+++ cutoff-common/src/lib.rs
@@ -8,10 +8,26 @@ pub mod logging;
 use std::thread;
 use std::thread::JoinHandle;
 
+pub trait IntoOk
+where
+    Self: Sized
+{
+    fn into_ok<E>(self) -> Result<Self, E>;
+}
+
 pub trait MaybeFrom<T> {
     fn maybe_from(value: T) -> Option<Self>
     where
         Self: Sized;
+}
+
+impl<T> IntoOk for T
+where
+    Self: Sized,
+{
+    fn into_ok<E>(self) -> Result<Self, E> {
+        Ok(self)
+    }
 }
 
 /// Shortcut function for `thread::Builder::new().name(name.into()).spawn(f).unwrap()`.
